5da136358483902e197ed302d7c274de2c665431
zena.kelz <zena.kelz@jumo.world>
2019-03-14 18:44:01 +0200

    Integrate Celery

--- simple_flaskapp_to_test_celery.py
+++ simple_flaskapp_to_test_celery.py
@@ -1,22 +1,31 @@
 """
 This is a simple app created with the purpose of Testing the Celery "retry" method
 
-Documentation for app creation:
+Create simple flask app
 http://flask.pocoo.org/docs/1.0/quickstart/
 
-Steps to Run:
-1) export FLASK_APP=simple_flaskapp_to_test_celery.py
-2) export FLASK_ENV=development
-3) flask run
+Integrate Celery
+http://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html#first-steps
+
+How to start up app and workers:
+
+# run web
+$ FLASK_APP=simple_flaskapp_to_test_celery.py FLASK_ENV=development  flask run
+
+# run workers
+$ celery -A tasks worker --loglevel=info
+
 """
 
 from flask import Flask
 from flask import jsonify
+from tasks import add
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    add.delay(4, 4)
     return jsonify({
         'Running Flask Application': 'simple_flaskapp_to_test_celery',
         'Message': 'Hello world!'})
