9a284cc24080d0e1d806f8e24585ffd2d6f8fb21
zena.kelz <zena.kelz@jumo.world>
2019-03-14 16:42:01 +0200

    Update comments and formatting

--- simple_flaskapp_to_test_celery.py
+++ simple_flaskapp_to_test_celery.py
@@ -1,8 +1,14 @@
-# This is a simple app created with the purpose of Testing Celery retry methods
-# http://flask.pocoo.org/docs/1.0/quickstart/
-# Steps to Run 1) export FLASK_APP=simple_flaskapp_to_test_celery.py
-#			   2) export FLASK_ENV=development
-# 		       3) flask run
+"""
+This is a simple app created with the purpose of Testing the Celery "retry" method
+
+Documentation for app creation:
+http://flask.pocoo.org/docs/1.0/quickstart/
+
+Steps to Run:
+1) export FLASK_APP=simple_flaskapp_to_test_celery.py
+2) export FLASK_ENV=development
+3) flask run
+"""
 
 from flask import Flask
 app = Flask(__name__)
