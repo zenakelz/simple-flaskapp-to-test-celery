3dda2c06b27548391c9e36040948a03fa0dbbd50
zena.kelz <zena.kelz@jumo.world>
2019-03-16 19:52:24 +0200

    Adding additional configuration to test celery
    + Test importing from a config file
    + Test using a dictionary to retrieve max_retries and countdown in the retry method
    + Test additional paramaters in task dictionary
    
    Also tested:
    Allocating a single worker to more than four queues
    >> celery -A tasks worker --loglevel=info --queue celery,foo,bar,baz,one,two,three
    (An error was previously identified when using more than 4 queues per worker)
    
    Outcome: Retry functionality works in all cases

---
+++ celeryconfig.py
@@ -0,0 +1,12 @@
+result_expires = 30
+timezone = 'UTC'
+
+accept_content = ['json', 'msgpack', 'yaml']
+task_serializer = 'json'
+result_serializer = 'json'
+
+task_ignore_result = True
+task_store_errors_even_if_ignored = True
+task_time_limit = 120
+task_soft_time_limit = 60
+
--- tasks.py
+++ tasks.py
@@ -1,23 +1,54 @@
 from celery import Celery
+import sqlalchemy
+import celeryconfig
 
 app = Celery('tasks', broker='redis://localhost:6379/0')
+app.config_from_object(celeryconfig)
 
+CELERY_DEFAULT_RETRY_DELAY = 5
+CELERY_DEFAULT_AUTO_RETRY_MAX = 8
+CELERY_RETRY_CONFIG = {
+    'STALE_DATA_ERROR': {
+        'retry_delay': 3,
+        'max_retries': 5
+    },
+    'POINTS_INTEGRITY_EXCEPTION': {
+        'retry_delay': 3,
+        'max_retries': 1
+    },
+    'POINTS_REPEATABLE_READ_EXCEPTION': {
+        'retry_delay': 0,
+        'max_retries': 5
+    }
+}
 
 @app.task
 def send_message(message):
     print(message)
 
 
-@app.task(bind=True)  # The bind argument to the task decorator will give access to self (the task type instance).
+# @app.task(bind=True)  # The bind argument to the task decorator will give access to self (the task type instance).
+
+@app.task(
+    bind=True,
+    autoretry_for=(
+        sqlalchemy.exc.InternalError,
+    ),
+    retry_kwargs={
+        'max_retries': CELERY_DEFAULT_AUTO_RETRY_MAX
+    },
+    default_retry_delay=CELERY_DEFAULT_RETRY_DELAY,
+)
 def send_message_with_retry(self, message):
     try:
         print(message)
         raise ValueError("Force retry")
     except ValueError as e:
         print(f"Retry count: {send_message_with_retry.request.retries}; Max retries set at: {send_message_with_retry.max_retries}")
+        print(f"POINTS retry_delay: {CELERY_RETRY_CONFIG['POINTS_REPEATABLE_READ_EXCEPTION']['retry_delay']}; max_retries: {CELERY_RETRY_CONFIG['POINTS_REPEATABLE_READ_EXCEPTION']['max_retries']}")
         # At this point the max_retries can be updated
         raise self.retry(
-            countdown=1,
+            countdown=CELERY_RETRY_CONFIG['POINTS_REPEATABLE_READ_EXCEPTION']['retry_delay'],
             exc=e,
-            max_retries=5
+            max_retries=CELERY_RETRY_CONFIG['POINTS_REPEATABLE_READ_EXCEPTION']['max_retries']
         )
